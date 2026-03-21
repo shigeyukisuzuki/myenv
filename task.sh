c1101ecb2844a5fe1379bf784f2a8278d5abf5d7
shigeyukisuzuki <kyuuritosuika@gmail.com>
2026-03-21 17:56:32 +0900

    improve task command to add prompt doing push/pull after dry run

--- task.sh
+++ task.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/bash
-if [ "$1" = "push" ]; then
-	if [ "$2" = "--no-dry" ]; then
-		rsync -av ~/task/ "${USER}@${taskServer}":~/task --update --delete
-	else
-		rsync -av ~/task/ "${USER}@${taskServer}":~/task --update --delete --dry-run
-	fi
-elif [ "$1" = "pull" ]; then
-	if [ "$2" = "--no-dry" ]; then
-		rsync -av "${USER}@${taskServer}":~/task/ ~/task --update --delete
-	else
-		rsync -av "${USER}@${taskServer}":~/task/ ~/task --update --delete --dry-run
-	fi
-else
-	echo "usage:"
-	echo "task push [--no-dry]"
-	echo "task pull [--no-dry]"
-fi
+
+case "$1" in
+	"push")
+		if [ "$2" = "--no-dry" ]; then
+			rsync -av ~/task/ "${USER}@${taskServer}":~/task --update --delete
+		else
+			rsync -av ~/task/ "${USER}@${taskServer}":~/task --update --delete --dry-run
+			read -p "sync remote data? [Y/n]: " x
+			if [ "$x" == 'Y' ]; then
+				rsync -av ~/task/ "${USER}@${taskServer}":~/task --update --delete
+			fi
+		fi
+		;;
+	"pull")
+		if [ "$2" = "--no-dry" ]; then
+			rsync -av "${USER}@${taskServer}":~/task/ ~/task --update --delete
+		else
+			rsync -av "${USER}@${taskServer}":~/task/ ~/task --update --delete --dry-run
+			read -p "sync local data? [Y/n]: " x
+			if [ "$x" == 'Y' ]; then
+				rsync -av "${USER}@${taskServer}":~/task/ ~/task --update --delete
+			fi
+		fi
+		;;
+	*)
+		echo "usage:"
+		echo "task push [--no-dry]"
+		echo "task pull [--no-dry]"
+		;;
+esac
