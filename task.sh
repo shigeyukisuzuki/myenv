9d535e8f358cf79d7b47b7fe390cac10072049b3
shigeyukisuzuki <kyuuritosuika@gmail.com>
2026-05-04 13:20:43 +0900

    exclude working directory

--- task.sh
+++ task.sh
@@ -3,23 +3,23 @@
 case "$1" in
 	"push")
 		if [ "$2" = "--no-dry" ]; then
-			rsync -av -e "ssh -i ${taskCredential}" ~/task/ "${USER}@${taskServer}":~/task --update --delete
+			rsync -av -e "ssh -i ${taskCredential}" ~/task/ "${USER}@${taskServer}":~/task --exclude='#work' --update --delete
 		else
-			rsync -av -e "ssh -i ${taskCredential}" ~/task/ "${USER}@${taskServer}":~/task --update --delete --dry-run
+			rsync -av -e "ssh -i ${taskCredential}" ~/task/ "${USER}@${taskServer}":~/task --exclude='#work' --update --delete --dry-run
 			read -p "sync remote data? [Y/n]: " x
 			if [ "$x" == 'Y' ]; then
-				rsync -av -e "ssh -i ${taskCredential}" ~/task/ "${USER}@${taskServer}":~/task --update --delete
+				rsync -av -e "ssh -i ${taskCredential}" ~/task/ "${USER}@${taskServer}":~/task --exclude='#work' --update --delete
 			fi
 		fi
 		;;
 	"pull")
 		if [ "$2" = "--no-dry" ]; then
-			rsync -av -e "ssh -i ${taskCredential}" "${USER}@${taskServer}":~/task/ ~/task --update --delete
+			rsync -av -e "ssh -i ${taskCredential}" "${USER}@${taskServer}":~/task/ ~/task --exclude='#work' --update --delete
 		else
-			rsync -av -e "ssh -i ${taskCredential}" "${USER}@${taskServer}":~/task/ ~/task --update --delete --dry-run
+			rsync -av -e "ssh -i ${taskCredential}" "${USER}@${taskServer}":~/task/ ~/task --exclude='#work' --update --delete --dry-run
 			read -p "sync local data? [Y/n]: " x
 			if [ "$x" == 'Y' ]; then
-				rsync -av -e "ssh -i ${taskCredential}" "${USER}@${taskServer}":~/task/ ~/task --update --delete
+				rsync -av -e "ssh -i ${taskCredential}" "${USER}@${taskServer}":~/task/ ~/task --exclude='#work' --update --delete
 			fi
 		fi
 		;;
