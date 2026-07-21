622f7c72b6cee38aa3b5a57a702a0bd861ea189b
Suzuki Shigeyuki <kyuuritosuika@gmail.com>
2026-07-21 21:45:03 +0900

    made vim applet and the launch mechanism

---
+++ vim_on_tmux.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+if [ $(pgrep -f gnome-terminal-server | wc -l) -eq 0 ]; then
+	# if gnome terminal is not running
+	gnome-terminal -- tmux new-session -s main -n "#vi" vim $1
+else
+	# if gnome terminal is already running
+	tmux new-window -n "#vi" vim $1
+fi
+
+# move terminal to most front
+xdotool windowactivate $(xdotool search --class "Gnome-terminal" | tail -1)
+#wmctrl -r :ACTIVE: -b add,above
