e128b6ad04e66d57454278a432c0ce381071cd9a
Suzuki Shigeyuki <kyuuritosuika@gmail.com>
2026-07-21 22:02:49 +0900

    add tmux status line style setting

--- vim_on_tmux.sh
+++ vim_on_tmux.sh
@@ -2,17 +2,10 @@
 
 if [ $(pgrep -f gnome-terminal-server | wc -l) -eq 0 ]; then
 	# if gnome terminal is not running
-<<<<<<< HEAD
-	gnome-terminal -- tmux new-session -s main -n "#vi" vim $1
-else
-	# if gnome terminal is already running
-	tmux new-window -n "#vi" vim $1
-=======
 	gnome-terminal -- tmux new-session -s main -n "/vi" vim $1
 else
 	# if gnome terminal is already running
 	tmux new-window -n "/vi" vim $1
->>>>>>> 2721997 (made vim applet and the launch mechanism)
 fi
 
 # move terminal to most front
