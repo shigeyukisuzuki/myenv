33ae5017aeec0ab75feb765a39282c0963b26067
shigeyukisuzuki <kyuuritosuika@gmail.com>
2026-07-24 21:32:34 +0900

    introduce rsync exclude file .rsyncignore

--- task.sh
+++ task.sh
@@ -3,23 +3,23 @@
 case "$1" in
 	"push")
 		if [ "$2" = "--no-dry" ]; then
-			rsync -av -e "ssh -i ${taskCredential}" ~/task/ "${USER}@${taskServer}":~/task --exclude='#work' --update --delete
+			rsync -av --exclude-from="${HOME}/task/.rsyncignore" -e "ssh -i ${taskCredential}" ~/task/ "${USER}@${taskServer}":~/task --update --delete
 		else
-			rsync -av -e "ssh -i ${taskCredential}" ~/task/ "${USER}@${taskServer}":~/task --exclude='#work' --update --delete --dry-run
+			rsync -av -e "ssh -i ${taskCredential}" --exclude-from="${HOME}/task/.rsyncignore" ~/task/ "${USER}@${taskServer}":~/task --update --delete --dry-run
 			read -p "sync remote data? [Y/n]: " x
 			if [ "$x" == 'Y' ]; then
-				rsync -av -e "ssh -i ${taskCredential}" ~/task/ "${USER}@${taskServer}":~/task --exclude='#work' --update --delete
+				rsync -av --exclude-from="${HOME}/task/.rsyncignore" -e "ssh -i ${taskCredential}" ~/task/ "${USER}@${taskServer}":~/task --update --delete
 			fi
 		fi
 		;;
 	"pull")
 		if [ "$2" = "--no-dry" ]; then
-			rsync -av -e "ssh -i ${taskCredential}" "${USER}@${taskServer}":~/task/ ~/task --exclude='#work' --update --delete
+			rsync -av --exclude-from="${HOME}/task/.rsyncignore" -e "ssh -i ${taskCredential}" "${USER}@${taskServer}":~/task/ ~/task --update --delete
 		else
-			rsync -av -e "ssh -i ${taskCredential}" "${USER}@${taskServer}":~/task/ ~/task --exclude='#work' --update --delete --dry-run
+			rsync -av --exclude-from="${HOME}/task/.rsyncignore" -e "ssh -i ${taskCredential}" "${USER}@${taskServer}":~/task/ ~/task --update --delete --dry-run
 			read -p "sync local data? [Y/n]: " x
 			if [ "$x" == 'Y' ]; then
-				rsync -av -e "ssh -i ${taskCredential}" "${USER}@${taskServer}":~/task/ ~/task --exclude='#work' --update --delete
+				rsync -av --exclude-from="${HOME}/task/.rsyncignore" -e "ssh -i ${taskCredential}" "${USER}@${taskServer}":~/task/ ~/task --update --delete
 			fi
 		fi
 		;;
